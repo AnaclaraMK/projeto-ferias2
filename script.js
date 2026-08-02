f0dc8433cf1290c4987855c15a1f211e18aaa899
Ana :3 <ana.daga@escola.pr.gov.br>
2026-08-02 04:55:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const botaoTopo = document.getElementById("btnTopo");
+
+window.addEventListener("scroll", () -> {
+    if (window.scrollY > 300) {
+        botaoTopo.classList.add("mostrar");    
+    } else {
+        botaoTopo.classList.remove("mostrar"); 
+    }
+});
 
