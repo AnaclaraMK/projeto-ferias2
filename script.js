cf991c5086b1a7946cba32aa06d8bacbe4811b77
Ana :3 <ana.daga@escola.pr.gov.br>
2026-08-02 17:33:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ window.addEventListener("DOMContentLoaded", () => {
     }, 5000);
     
 });
+const btnTema = document.getElementById("btn-tema");
+const corpoPagina = document.body;
+
+if (localStorage.getItem("tema") === "dark") {
+    corpoPagina.classList.add("dark-mode");
+    btnTema.innerHTML = "&#9789;"; 
+}
+
+btnTema.addEventListener("click", () => {
+    corpoPagina.classList.toggle("dark-mode");
+
+    if (corpoPagina.classList.contains("dark-mode")) {
+        btnTema.innerHTML = "&#9789;"; 
+        localStorage.setItem("tema", "dark");
+    } else {
+        btnTema.innerHTML = "&#9788;";
+        localStorage.setItem("tema", "light");
+    }
+});
