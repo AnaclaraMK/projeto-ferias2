a120552ad4c89db8b2730b86c3644e50b3eb59e8
Ana :3 <ana.daga@escola.pr.gov.br>
2026-08-02 15:41:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const botaoFixo = document.getElementById("btnFixoContato");
 botaoFixo.addEventListener("click", () => {
     window.location.hash = "#";
 });
+window.addEventListener("DOMContentLoaded", () => {
+    
+    const alerta = document.getElementById("alerta-boas-vindas");
+
+    setTimeout(() => {
+        alerta.classList.add("mostrar-alerta");
+    }, 1000);
+
+    setTimeout(() => {
+        alerta.classList.remove("mostrar-alerta");
+    }, 5000);
+    
+});
